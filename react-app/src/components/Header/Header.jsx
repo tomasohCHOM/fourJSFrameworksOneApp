54b677e8b75d3cbc3270b591af0c4aec1fbcaeb4
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-09-25 10:43:20 -0700

    Initial template for react app

---
+++ react-app/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.container}>
+      <div className={styles.container.left}>Tomas Oh</div>
+      <div className={styles.right}>
+        <span>About</span>
+        <span>Projects</span>
+        <span>Contact</span>
+        <button>Dark Mode</button>
+      </div>
+    </header>
+  );
+}
