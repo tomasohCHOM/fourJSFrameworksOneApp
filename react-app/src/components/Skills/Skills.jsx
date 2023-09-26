b6cf88d671511b00c6a994f26fd0a3ed2a2b851a
Oh <tomasoh@csu.fullerton.edu>
2023-09-26 14:40:31 -0700

    Displaying images

--- react-app/src/components/Skills/Skills.jsx
+++ react-app/src/components/Skills/Skills.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./Skills.module.css";
 
 export default function Skills() {
   const programmingSkills = [
@@ -22,14 +23,28 @@ export default function Skills() {
 
   return (
     <section>
-      <h2>Skills</h2>
-      <div>
+      <h2 className={styles.subtitle}>Skills</h2>
+      <div className={styles.programmingSkillsContainer}>
+        <h3>Programming</h3>
         {programmingSkills.map((skill, key) => {
-          console.log(`../../assets/skills/${skill}-icon.png`);
+          // console.log(`../../assets/skills/${skill}-icon.png`);
           return (
             <img
               key={key}
-              src={`../../assets/skills/${skill}-icon.png`}
+              src={`/assets/skills/${skill}-icon.png`}
+              alt={skill + " icon"}
+            />
+          );
+        })}
+      </div>
+      <div className={styles.frameworkSkillsContainer}>
+        <h3>Framework</h3>
+        {frameworkSkills.map((skill, key) => {
+          // console.log(`../../assets/skills/${skill}-icon.png`);
+          return (
+            <img
+              key={key}
+              src={`/assets/skills/${skill}-icon.png`}
               alt={skill + " icon"}
             />
           );
