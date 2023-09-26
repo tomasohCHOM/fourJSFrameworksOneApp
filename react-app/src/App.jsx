f708349bfa922aacd770a6c2e669b30c54392259
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-09-25 19:17:24 -0700

    Changed theme, added Hero component

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <>
-      <Header></Header>
+      <Header />
+      <Hero />
     </>
   );
 }
---
+++ react-app/src/components/Hero/Hero.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <>
+      <div>Hello! My name is</div>
+      <h1>Tomas Oh</h1>
+    </>
+  );
+}
