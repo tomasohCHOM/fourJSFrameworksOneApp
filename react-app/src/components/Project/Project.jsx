c60b4eab43be7ebd0bc6ff5f96f713009e027eb6
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-09-28 11:15:22 -0700

    Started Project and ProjectsList components

--- react-app/src/components/Project/Project.jsx
+++ react-app/src/components/Project/Project.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import styles from "./Project.module.css";
 
-export default function Project() {
-  return <div>Project</div>;
+export default function Project(props) {
+  return (
+    <div>
+      <h3>{props.projectName}</h3>
+      <img
+        className={styles.image}
+        src={props.imageSource}
+        alt={"Image for Project " + props.projectName}
+      />
+      <div className={styles.techStackContainer}>
+        {props.projectTechStack.map((technology) => {
+          return <Badge text={technology} />;
+        })}
+      </div>
+      <p>{props.projectDescription}</p>
+    </div>
+  );
 }
--- react-app/src/components/ProjectsList/ProjectsList.jsx
+++ react-app/src/components/ProjectsList/ProjectsList.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import styles from "./ProjectsList.module.css";
 
 export default function ProjectsList() {
   return (
     <section>
       <h2>Projects</h2>
+      <div className={styles.projectsListContainer}></div>
     </section>
   );
 }
