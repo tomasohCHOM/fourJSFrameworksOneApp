e11ea6c59167c507f59ca4011f3000e30cb35a48
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-10-10 11:20:14 -0700

    Started button component

---
+++ react-app/src/components/Button/Button.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+export default function Button(props) {
+  return <button className={styles.button}>{props.text}</button>;
+}
