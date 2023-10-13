d2d9b535834b1d2aa106f3138d17fc4ba57342df
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-10-13 15:57:51 -0700

    Completed the navbar

--- react-app/src/components/Navbar/Navbar.jsx
+++ react-app/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import styles from "./Navbar.module.css";
+import { FaBars } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
 
 const navbarItems = [
   { title: "About", path: "#about" },
@@ -14,23 +16,41 @@ const goTo = (event, path) => {
 };
 
 export default function Navbar() {
-  const [open, setOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className={styles.container}>
-      <div className={styles.container.left}>Tomas Oh</div>
-      <div className={styles.right}>
-        <div className={`${styles.sections} ${open && styles.active}`}>
-          {navbarItems.map((item, index) => (
-            <span key={index} onClick={(e) => goTo(e, item.path)}>
-              {item.title}
-            </span>
-          ))}
+    <>
+      <nav className={styles.container}>
+        <div className={styles.container.left}>
+          <span>Tomas Oh</span>
         </div>
-        <button onClick={() => setOpen(!open)} className={styles.openNavbarBtn}>
-          X
-        </button>
-      </div>
-    </nav>
+        <div className={styles.right}>
+          <div className={`${styles.sections} ${isOpen && styles.active}`}>
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className={styles.closeNavbarBtn}
+            >
+              <RxCross2 />
+            </button>
+            {navbarItems.map((item, index) => (
+              <span key={index} onClick={(e) => goTo(e, item.path)}>
+                {item.title}
+              </span>
+            ))}
+          </div>
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className={styles.openNavbarBtn}
+          >
+            <FaBars />
+          </button>
+        </div>
+      </nav>
+
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+        className={`${styles.darkCover} ${isOpen && styles.activeCover}`}
+      />
+    </>
   );
 }
