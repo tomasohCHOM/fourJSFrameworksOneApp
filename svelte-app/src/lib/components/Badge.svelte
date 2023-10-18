e383ea50138a245ee585d1bef1a812f561208f60
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-10-17 19:40:24 -0700

    Created Badge SvelteApp

---
+++ svelte-app/src/lib/components/Badge.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let text;
+</script>
+
+<span class="badge">
+  {text}
+</span>
+
+<style>
+  .badge {
+    cursor: pointer;
+    padding: 0.25rem 0.5rem;
+    color: var(--color-blue);
+    border: 2px solid var(--color-blue);
+    border-radius: 1rem;
+    font-weight: 600;
+  }
+</style>
