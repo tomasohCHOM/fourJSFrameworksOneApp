9c66d19bbe1e7bad0a2ca878da7164434f92ce29
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-10-19 11:16:00 -0700

    Started projects in vue

---
+++ vue-app/src/components/Project.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="projectContainer">
+    <h3>{{ project.name }}</h3>
+    <img
+      class="image"
+      :src="project.imageSource"
+      :alt="'Image for Project ' + project.projectName"
+    />
+    <div class="techStackContainer">
+      <Badge v-for="technology in project.techStack" :text="technology" />
+    </div>
+    <p>{{ project.description }}</p>
+    <div class="links">
+      <Button v-if="project.demoLink !== ''">
+        <a class="link" :href="project.demoLink">Demo</a>
+      </Button>
+      <Button>
+        <a class="link" :href="project.githubLink"> Github Repo </a>
+      </Button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Badge from "./Badge.svelte";
+import Button from "./Button.svelte";
+
+export default {
+  name: "Project",
+  props: {
+    project: Object,
+  },
+  components: {
+    Badge,
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+.projectContainer {
+  display: flex;
+  align-items: center;
+  /* justify-content: center; */
+  flex-direction: column;
+  row-gap: 0.5rem;
+  padding: 2rem;
+  box-shadow: var(--box-shadow);
+  border-radius: 1.5rem;
+  width: auto;
+}
+
+.projectContainer > h3 {
+  font-weight: 600;
+}
+
+.projectContainer > p {
+  text-align: center;
+  width: 80%;
+}
+
+.link {
+  font-family: "DM Sans", sans-serif;
+  font-size: 1rem;
+  text-decoration: none;
+  color: rgb(255, 255, 255);
+}
+
+.image {
+  width: 80%;
+  /* height: 15rem; */
+}
+
+.techStackContainer {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+}
+
+@media only screen and (max-width: 40rem) {
+  .projectContainer {
+    padding: 1.5rem 2rem;
+  }
+
+  .image {
+    height: auto;
+  }
+}
+</style>
---
+++ vue-app/src/components/ProjectsList.vue
@@ -0,0 +1,78 @@
+<template>
+  <section>
+    <h2 id="projects" class="styles">Projects</h2>
+    <div class="projectsListContainer">
+      <Project v-for="project in projectsList" :project="project" />
+    </div>
+  </section>
+</template>
+
+<script>
+import Project from "./Project.vue";
+
+const projectsList = [
+  {
+    name: "Clubannounce",
+    demoLink: "",
+    githubLink: "https://github.com/acmcsufoss/clubannounce",
+    imageSource: "/assets/projects/clubannounce.png",
+    techStack: ["SvelteKit", "TypeScript", "Sass", "Figma", "API"],
+    description:
+      "A Web page aimed to speed the process of creating new announcements and events within ACM, the largest Computer Science club at Cal State Fullerton.",
+  },
+  {
+    name: "Devdle",
+    demoLink: "https://devdlegame.vercel.app",
+    githubLink: "https://github.com/tomasohCHOM/Devdle",
+    imageSource: "/assets/projects/devdle.png",
+    techStack: ["SvelteKit", "TypeScript", "Sass", "Supabase", "OAuth"],
+    description:
+      "A Wordle clone based on Computer Science/Web Development concepts. Its goal is to entertain and educate users who want to get into the Software Engineering field.",
+  },
+  {
+    name: "MatriXpert",
+    demoLink: "https://matrixpert.netlify.app",
+    githubLink: "https://github.com/tomasohCHOM/MatriXpert",
+    imageSource: "/assets/projects/matrixpert.png",
+    techStack: ["React", "React Router", "Sass", "Katex", "Vitest"],
+    description:
+      "A Matrix calculator website that supports operations such as Matrix multiplication, transpose, determinant, and REF/RREF.",
+  },
+  {
+    name: "CanvasFindPeople",
+    demoLink: "",
+    githubLink: "https://github.com/tomasohCHOM/CanvasFindPeople",
+    imageSource: "/assets/projects/canvas-find-people.png",
+    techStack: ["Python", "SQLite", "Discord Bot", "Canvas API"],
+    description:
+      "A Discord bot made in Python that makes use of the Canvas API to search for users in Canvas courses.",
+  },
+];
+
+export default {
+  name: "ProjectsList",
+  data: () => {
+    return {
+      projectsList,
+    };
+  },
+  components: {
+    Project,
+  },
+};
+</script>
+
+<style scoped>
+.projectsListContainer {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1.5rem;
+  margin-block-end: 2rem;
+}
+
+@media only screen and (max-width: 60rem) {
+  .projectsListContainer {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
